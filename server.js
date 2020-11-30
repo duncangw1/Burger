5dc4c7fe70c50564969b1e27dabb336e27165ee8
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-11-30 14:38:44 -0500

    Began view setup and began working on bootstrap and css styling.

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
 // Dependencies
 const express = require("express");
+
+// Allows method in index.handlebars to function correctly
+const methodOverride = require("method-override");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+// Setting main page to always display
+app.use(methodOverride("_method"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
