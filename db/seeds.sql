57d14804c53abb1c3ef53ef44a38e1f24865e3e3
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-11-30 10:53:17 -0500

    Completed database setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Onionator');
+
+INSERT INTO burgers (burger_name) VALUES ('Comeback Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Express Burger');
