c1a75c53a9d0fdddc3a6b077c6d64e5e7d67448d
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-11-30 12:05:59 -0500

    Completed controller setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+// Dependencies
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (burger.js) to use its database functions
+var burger = require("../models/burger.js");
+
+// Create all of our routes and set up logic within those routes
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    let hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burgers", (req, res) => {
+  burger.insertOne(["burger_name"], [req.body.burger_name], () => {
+    res.redirect("/");
+  });
+});
+
+router.put("/burgers/:id", (req, res) => {
+  let condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne(
+    {
+      devoured: true,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.redirect("/");
+    }
+  );
+});
+
+// Export routes for server.js to use
+module.exports = router;
